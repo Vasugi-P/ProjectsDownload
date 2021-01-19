5a055cae06132bedd134cf23ee5a2187b0268b0b
Vasugi-P <41140455+Vasugi-P@users.noreply.github.com>
2021-01-19 12:17:48 +0530

    Add files via upload

---
+++ Product/src/main/java/com/product/service/Impl/RatingServiceImpl.java
@@ -0,0 +1,43 @@
+package com.product.service.Impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.product.dto.RatingDto;
+import com.product.entity.Rating;
+import com.product.exception.ProductNotFoundException;
+import com.product.exception.UserNotFoundException;
+import com.product.repo.RatingRepo;
+
+import com.product.service.RatingService;
+
+
+@Service
+public class RatingServiceImpl implements RatingService{
+	
+	@Autowired
+	private RatingRepo rp;
+	
+	
+	@Override
+	public Rating giveRatingFeedback(RatingDto ratingDto)throws ProductNotFoundException,UserNotFoundException{
+		Rating rate=new Rating(ratingDto.getProductId(),ratingDto.getUserId(),ratingDto.getSellerFeedback(),ratingDto.getUserFeedback(),
+				ratingDto.getSellerRating(),ratingDto.getUserRating());
+		return rp.save(rate);			
+		
+	}
+
+
+	@Override
+	public void deleteRating(Long ratingId) {
+		rp.deleteById(ratingId);
+	}
+
+
+//	@Override
+//	public Rating getRatingByRatingId(Long ratingId) {
+//		Rating rate=rp.findById(ratingId);
+//				return rate;
+//	}
+
+}
---
+++ Product/src/main/java/com/product/service/RatingService.java
@@ -0,0 +1,21 @@
+package com.product.service;
+
+import com.product.dto.RatingDto;
+import com.product.entity.Rating;
+import com.product.exception.ProductNotFoundException;
+import com.product.exception.UserNotFoundException;
+
+public interface RatingService {
+	
+	public Rating giveRatingFeedback(RatingDto ratingDto)throws ProductNotFoundException,UserNotFoundException;
+
+	public void deleteRating(Long ratingId);
+	
+	//Rating getRatingByRatingId(Long ratingId);
+	
+
+	
+
+	
+
+}
